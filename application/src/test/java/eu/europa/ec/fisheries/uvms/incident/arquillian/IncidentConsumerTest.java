259816c609eac0d610d93efd597927c0975ef654
Kasim <kasimgul@gmail.com>
2019-12-19 13:13:12 +0100

    Adding new integration tests

--- application/src/test/java/eu/europa/ec/fisheries/uvms/incident/arquillian/IncidentConsumerTest.java
+++ application/src/test/java/eu/europa/ec/fisheries/uvms/incident/arquillian/IncidentConsumerTest.java
@@ -1,11 +1,11 @@
 package eu.europa.ec.fisheries.uvms.incident.arquillian;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import eu.europa.ec.fisheries.schema.movementrules.ticket.v1.TicketStatusType;
 import eu.europa.ec.fisheries.schema.movementrules.ticket.v1.TicketType;
-import eu.europa.ec.fisheries.uvms.incident.TransactionalTests;
+import eu.europa.ec.fisheries.uvms.incident.BuildIncidentTestDeployment;
+import eu.europa.ec.fisheries.uvms.incident.helper.JMSHelper;
+import eu.europa.ec.fisheries.uvms.incident.helper.TicketHelper;
+import eu.europa.ec.fisheries.uvms.incident.helper.TopicListener;
 import eu.europa.ec.fisheries.uvms.incident.service.domain.dto.IncidentDto;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -16,44 +16,36 @@ import org.junit.runner.RunWith;
 import javax.inject.Inject;
 import javax.jms.Message;
 import javax.jms.TextMessage;
-import java.time.Instant;
 import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Arquillian.class)
-public class IncidentConsumerTest extends TransactionalTests {
+public class IncidentConsumerTest extends BuildIncidentTestDeployment {
 
     @Inject
     private JMSHelper jmsHelper;
 
-    private static final String QUEUE_NAME = "IncidentEvent";
-    public static final String EVENT_STREAM = "jms.topic.EventStream";
+    @Inject
+    private TicketHelper ticketHelper;
 
-    private static ObjectMapper objectMapper = new ObjectMapper();
-
-    static {
-        objectMapper
-                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .findAndRegisterModules();
-    }
+    private ObjectMapper objectMapper = getObjectMapper();
 
     @Before
     public void clearExchangeQueue() throws Exception {
-        jmsHelper.clearQueue(QUEUE_NAME);
+        jmsHelper.clearQueue(jmsHelper.QUEUE_NAME);
     }
 
     @Test
     @OperateOnDeployment("incident")
     public void consumeIncidentQueue() throws Exception {
-        String ticketId = UUID.randomUUID().toString();
-        String assetId = UUID.randomUUID().toString();
-        String movId = UUID.randomUUID().toString();
-        String mobTermId = UUID.randomUUID().toString();
-        TicketType ticket = createTicket(ticketId, assetId, movId, mobTermId);
+        UUID ticketId = UUID.randomUUID();
+        UUID assetId = UUID.randomUUID();
+        UUID movId = UUID.randomUUID();
+        UUID mobTermId = UUID.randomUUID();
+        TicketType ticket = ticketHelper.createTicket(ticketId, assetId, movId, mobTermId);
 
-        try (TopicListener listener = new TopicListener(EVENT_STREAM, "")) {
+        try (TopicListener listener = new TopicListener(jmsHelper.EVENT_STREAM, "")) {
             String asString = objectMapper.writeValueAsString(ticket);
             jmsHelper.sendMessageToIncidentQueue(asString);
 
@@ -62,24 +54,7 @@ public class IncidentConsumerTest extends TransactionalTests {
 
             String text = textMessage.getText();
             IncidentDto incident = objectMapper.readValue(text, IncidentDto.class);
-            assertEquals(assetId, incident.getAssetId().toString());
+            assertEquals(assetId, incident.getAssetId());
         }
-    }
-
-    private TicketType createTicket(String ticketId, String assetId, String movId, String mobTermId) {
-        TicketType ticket = new TicketType();
-        ticket.setGuid(ticketId);
-        ticket.setAssetGuid(assetId);
-        ticket.setMovementGuid(movId);
-        ticket.setMobileTerminalGuid(mobTermId);
-        ticket.setRuleName("Asset not sending");
-        ticket.setRuleGuid("Asset not sending");
-        ticket.setUpdatedBy("UVMS");
-        ticket.setStatus(TicketStatusType.POLL_PENDING);
-        ticket.setTicketCount(1L);
-        String date = String.valueOf(Instant.now().getEpochSecond());
-        ticket.setOpenDate(date);
-        ticket.setUpdated(date);
-        return ticket;
     }
 }
---
+++ application/src/test/java/eu/europa/ec/fisheries/uvms/incident/helper/TicketHelper.java
@@ -0,0 +1,29 @@
+package eu.europa.ec.fisheries.uvms.incident.helper;
+
+import eu.europa.ec.fisheries.schema.movementrules.ticket.v1.TicketStatusType;
+import eu.europa.ec.fisheries.schema.movementrules.ticket.v1.TicketType;
+
+import javax.ejb.Stateless;
+import java.time.Instant;
+import java.util.UUID;
+
+@Stateless
+public class TicketHelper {
+
+    public TicketType createTicket(UUID ticketId, UUID assetId, UUID movId, UUID mobTermId) {
+        TicketType ticket = new TicketType();
+        ticket.setGuid(ticketId.toString());
+        ticket.setAssetGuid(assetId.toString());
+        ticket.setMovementGuid(movId.toString());
+        ticket.setMobileTerminalGuid(mobTermId.toString());
+        ticket.setRuleName("Asset not sending");
+        ticket.setRuleGuid("Asset not sending");
+        ticket.setUpdatedBy("UVMS");
+        ticket.setStatus(TicketStatusType.POLL_PENDING);
+        ticket.setTicketCount(1L);
+        String date = String.valueOf(Instant.now().getEpochSecond());
+        ticket.setOpenDate(date);
+        ticket.setUpdated(date);
+        return ticket;
+    }
+}
