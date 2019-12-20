1613fca0578c89971b262f789f39cf8dd1dd7f94
Kasim <kasimgul@gmail.com>
2019-12-20 11:09:53 +0100

    Incident update test

--- application/src/test/java/eu/europa/ec/fisheries/uvms/incident/arquillian/IncidentServiceBeanTest.java
+++ application/src/test/java/eu/europa/ec/fisheries/uvms/incident/arquillian/IncidentServiceBeanTest.java
@@ -6,7 +6,9 @@ import eu.europa.ec.fisheries.uvms.incident.TransactionalTests;
 import eu.europa.ec.fisheries.uvms.incident.helper.JMSHelper;
 import eu.europa.ec.fisheries.uvms.incident.helper.TicketHelper;
 import eu.europa.ec.fisheries.uvms.incident.service.bean.IncidentServiceBean;
+import eu.europa.ec.fisheries.uvms.incident.service.domain.dto.StatusDto;
 import eu.europa.ec.fisheries.uvms.incident.service.domain.entities.Incident;
+import eu.europa.ec.fisheries.uvms.incident.service.domain.enums.StatusEnum;
 import org.jboss.arquillian.container.test.api.OperateOnDeployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
@@ -69,6 +71,31 @@ public class IncidentServiceBeanTest extends TransactionalTests {
         assertNotNull(incident);
         assertEquals(assetId, incident.getAssetId());
         assertEquals(movementId, incident.getMovementId());
+    }
+
+    @Test
+    @OperateOnDeployment("incident")
+    public void updateIncidentTest() throws Exception {
+        UUID ticketId = UUID.randomUUID();
+        UUID assetId = UUID.randomUUID();
+        UUID movementId = UUID.randomUUID();
+        UUID mobTermId = UUID.randomUUID();
+        TicketType ticket = ticketHelper.createTicket(ticketId, assetId, movementId, mobTermId);
+        String asString = objectMapper.writeValueAsString(ticket);
+        jmsHelper.sendMessageToIncidentQueue(asString);
+
+        LockSupport.parkNanos(5000000000L);
+
+        Incident created = incidentService.findByTicketId(ticketId);
+        assertEquals(StatusEnum.POLL_FAILED, created.getStatus());
+
+        created.setStatus(StatusEnum.RESOLVED);
+        StatusDto status = new StatusDto();
+        status.setStatus("RESOLVED");
+        incidentService.updateIncidentStatus(created.getId(), status);
+
+        Incident updated = incidentService.findByTicketId(ticketId);
+        assertEquals(updated.getStatus(), StatusEnum.RESOLVED);
     }
 
 }
