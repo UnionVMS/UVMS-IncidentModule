92630f30c2684ef79076cc007945ec48c845934c
Jonas E.Mattsson <jonas.mattsson@consid.se>
2020-08-17 10:03:34 +0200

    implemented a loop for checking if a manual movement has not arrived in 65 minutes and sets a status stating that it is late.

--- model/src/main/java/eu/europa/ec/fisheries/uvms/incident/model/dto/enums/EventTypeEnum.java
+++ model/src/main/java/eu/europa/ec/fisheries/uvms/incident/model/dto/enums/EventTypeEnum.java
@@ -2,6 +2,7 @@ package eu.europa.ec.fisheries.uvms.incident.model.dto.enums;
 
 public enum EventTypeEnum {
     MANUAL_POSITION("New Manual position created", RelatedObjectType.MOVEMENT),
+    MANUAL_POSITION_LATE("No new manual position in 65 minutes while in manual position mode", RelatedObjectType.NONE),
     POLL_CREATED("Poll has been created", RelatedObjectType.POLL),
     AUTO_POLL_CREATED("Automatic poll has been created", RelatedObjectType.POLL),
     NOTE_CREATED("Note has been created", RelatedObjectType.NOTE),
