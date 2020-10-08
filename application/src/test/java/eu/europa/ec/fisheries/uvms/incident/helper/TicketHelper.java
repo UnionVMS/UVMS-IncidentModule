4cc85b564224ec77613d81c29ba44a48a9d4db9d
Jonas E.Mattsson <jonas.mattsson@consid.se>
2020-10-08 10:27:48 +0200

    removed dep on MR

--- application/src/test/java/eu/europa/ec/fisheries/uvms/incident/helper/TicketHelper.java
+++ application/src/test/java/eu/europa/ec/fisheries/uvms/incident/helper/TicketHelper.java
@@ -1,6 +1,5 @@
 package eu.europa.ec.fisheries.uvms.incident.helper;
 
-import eu.europa.ec.fisheries.schema.movementrules.ticket.v1.TicketStatusType;
 import eu.europa.ec.fisheries.uvms.incident.model.dto.IncidentDto;
 import eu.europa.ec.fisheries.uvms.incident.model.dto.IncidentTicketDto;
 import eu.europa.ec.fisheries.uvms.incident.model.dto.enums.IncidentType;
@@ -20,7 +19,6 @@ public class TicketHelper {
         ticket.setRuleName("Asset not sending");
         ticket.setRuleGuid("Asset not sending");
         ticket.setUpdatedBy("UVMS");
-        ticket.setStatus(TicketStatusType.POLL_PENDING.value());
         ticket.setTicketCount(1L);
         Instant date = Instant.now();
         ticket.setCreatedDate(date);
